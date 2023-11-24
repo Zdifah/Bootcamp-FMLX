81accd95e31e2befe1fb451f85bb1a9ced23e6da
Zdifah <ilhamhafidzfahry@gmail.com>
2023-11-25 05:55:53 +0700

    nambah materi

---
+++ Day7/Generic/Program.cs
@@ -0,0 +1,80 @@
+﻿class Program 
+{
+	static void Main()
+	{
+		/*
+		generic class dimana fieldnya atau parameter atau method yang bertipe T tidak
+		dapat melakukan operasi logika karna tipe T belum jelas dia bertipe apa. walaupun
+		kita sudah menetapakn class generic kita sebagai int tetap tidak bisa melakukan
+		operasi logika
+		*/
+		CustomCollection<int> custom = new CustomCollection<int>(20);
+		custom.Add(0, 1);
+		custom.Add(1, 3);
+		//custom.Add(2, "hello");
+		int result = custom.GetValue(1);
+		System.Console.WriteLine(result);
+		
+		CustomCollection<string> custom2 = new(2);
+		custom2.Add(0,"a");
+		
+		GenericList<int> genericList = new();
+		System.Console.WriteLine(genericList.Add<float>(9.89f));
+		//genericList.Sum(10, 5);// --> disini akan eror
+		genericList.Testing(10, 10); // --> disini akan eror
+		
+	}
+}
+/*
+generic memberikan kita kemudahan dalam membuat collection yang safe type
+seperti kita membuat kelas yang khusus menerima satu tipe data saja
+karna kita mau fleksibilitas yang tinggi maka generic dapat membantu
+pekerjaan anda
+*/
+// <T> --> tanda kalo class ini generic dan ketika dibuat instancenta
+// ex CustomCollection<int> test = new(); --> T dapat diganti dengan
+// berbagai macam tipe atau class
+class CustomCollection<T>
+{
+	// kelasnya bertipe generic lalu fieldnya atau methodnya
+	// bisa generic juga dengan manggantika tipe data dengan T
+	// simbol <T> hanya untuk insialisasi 
+	T[] myArray;
+	public CustomCollection(int arraySize)
+	{
+		myArray = new T[arraySize];
+	}
+	public bool Add(int index, T x)
+	{
+		myArray[index] = x;
+		return true;
+	}
+	public T GetValue(int index)
+	{
+		return myArray[index];
+	}
+}
+
+// generic class
+public class GenericList<T>
+{
+	// generic method, genericnya berbeda
+	public U Add<U>(U a)
+	{
+		return a;
+	}
+	public T Sum(T a, T b)
+	{
+		return a + b;
+	}
+	public bool Testing(T a, T b)
+	{
+		if(a == b)
+		{
+			return true;
+		}else
+		{
+			return false;
+		}
+	}
+}
---
+++ Day7/GenericConstraint/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+//Generic Constraint
+using System.Numerics;
+class Program
+{
+	static void Main()
+	{
+		/*
+		generic constrain memberikan kita kemudahan dalam fleksibilitas
+		dari batasan generic class. seperti jika generic class dan field
+		atau methodnya bertipe generic maka dia tidak bisa dilakukan
+		operasi logika, untuk mengatasi permaslahan itu generic constraint
+		menambah kapabilitas sehingga dapat melakukan operasi logika tersebut.
+		terdapat banyak jenis generic constraint. harus liat documentationnya
+		kalo mau tau pilihan generic constrainya apa saja.
+		*/
+		System.Console.WriteLine(Calculator<int>.Add(10, 5));
+		
+		// karna generic dapat menjadi tipe atau class apapun
+		// maka kita juga bisa melakukan operasi tambah pada class Car
+		Car a = new Car(5);
+		Car b = new Car(5);
+		System.Console.WriteLine(Calculator<Car>.Add(a, b).price);
+	}
+}
+// generic constraint dideklarasi dengan adanya "where T: bla bla"
+//IAdditionalOperators<T,T,T> -> class generic dapat melakukan operasi +
+class Calculator<T> where T: IAdditionOperators<T, T, T>
+{
+	public static T Add(T a, T b)
+	{
+		return a + b;
+	}
+}
+class Car : IAdditionOperators<Car, Car, Car>
+{
+	public int price;
+	public Car(int p)
+	{
+		price = p;
+	}
+	// ini namanya operator overloading, kita merubah format operator +
+	// sehinnga bisa melakukan penjumlahan dengan class Car
+	public static Car operator +(Car left, Car right)
+	{
+		return new Car(left.price + right.price);
+	}
+}
