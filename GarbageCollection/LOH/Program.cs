b010a00ddbe063ffcfc72655aa1484d6fd1e1385
Zdifah <ilhamhaifdzfahry@gmail.com>
2023-12-06 10:50:21 +0700

    tambah

---
+++ GarbageCollection/LOH/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LOHDemo
+{
+	/* 
+	jika ada object yang besar lebih dari 80kb maka dia akan ditempatkan
+	di LOH (large Object Heap) dia akan di hapus jika gen 2 di bersihkan
+	*/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float[] largeArray = new float[100 * 1024];  // 400 KB
+
+			Console.WriteLine("Large object allocated on the LOH.");
+			Console.ReadKey();
+		}
+	}
+}
