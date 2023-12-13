921ad61c07a2b332bc60e4a67e3edce78e76fe76
Zdifah <ilhamhaifdzfahry@gmail.com>
2023-12-13 10:20:54 +0700

    nambah

---
+++ Day20/XMLSerilization/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.XmlObjectSerializer;
+class Program
+{
+	static void Main()
+	{
+		InfoTerkini infoTerkini = new InfoTerkini("ilham", "tambun", 1);
+		
+		XmlSerializer saveInfo = new XmlSerializer(typeof(InfoTerkini));
+		
+		using (StreamWriter writer = new StreamWriter("person.xml"))
+		{
+			saveInfo.Serialize(writer, infoTerkini);
+		}
+		
+	}
+}
+
+[DataContract]
+public class InfoTerkini
+{
+	
+	public string siapa;
+	public string kemana;
+	public int berapa;
+	
+	public InfoTerkini(string siapa, string kemana, int berapa)
+	{
+		this.siapa = siapa;
+		this.kemana = kemana;
+		this.berapa = berapa;
+	}
+}
